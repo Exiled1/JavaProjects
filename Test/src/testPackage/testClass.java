a76173dfc3bf0bbbd96c94c7986e200d7a72a908
Benlion12 <wazzupbuddy34@gmail.com>
2018-08-31 10:03:41 -0700

    added a bunch of new stuffs onto my branch

---
+++ Test/src/testPackage/testClass.java
@@ -0,0 +1,16 @@
+package testPackage;
+
+/*
+ * 	Written By:
+ *  @Linuxxx / RudyPeralta
+ */
+
+public class testClass {
+	public static void main(String [] args) {
+		System.out.println("\ta\tb\tc");
+		System.out.println("\\\\");
+		System.out.println("'");
+		System.out.println("\"\"\"");
+		System.out.println("C:\nin\the downward spiral");
+		}
+}
