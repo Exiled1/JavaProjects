4091a963930b5f46bf255a674b8a5899646aae85
Benlion12 <wazzupbuddy34@gmail.com>
2018-09-05 09:58:45 -0700

    Updated notes with 9/5 notes, added NameLoop, added StarFigures

---
+++ StarFigures/src/starFigures/StarFigs.java
@@ -0,0 +1,47 @@
+package starFigures;
+
+public class StarFigs {
+
+	public static void main(String[] args) {
+		FiveStar();
+		FiveStar();
+		TwoStar();
+		OneStar();
+		TwoStar();
+		
+		System.out.println("");
+		System.out.println("");
+		
+		FiveStar();
+		FiveStar();
+		TwoStar();
+		OneStar();
+		TwoStar();
+		FiveStar();
+		FiveStar();
+		
+		System.out.println("");
+		System.out.println("");
+		
+		OneStar();
+		OneStar();
+		OneStar();
+		FiveStar();
+		FiveStar();
+		TwoStar();
+		OneStar();
+		TwoStar();
+	}
+	
+	public static void FiveStar() {
+		System.out.println("* * * * *");
+	}
+	
+	public static void TwoStar() {
+		System.out.println("  *   *  ");
+	}
+	
+	public static void OneStar() {
+		System.out.println("    *    ");
+	}
+}
