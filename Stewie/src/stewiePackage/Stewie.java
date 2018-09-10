f5f454ab9d8ce1c53b9c9834641e1b85abaecd92
Linuxxx <wazzupbuddy34@gmail.com>
2018-09-09 22:14:23 -0700

    Added a Stewie project, doesn't do anything other than output mini ASCII stuff

---
+++ Stewie/src/stewiePackage/Stewie.java
@@ -0,0 +1,9 @@
+package stewiePackage;
+
+public class Stewie {
+    public static void main(String[] args) {
+        System.out.println("//////////////////////");
+        System.out.println("|| Victory Is Mine  ||");
+        System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+    }
+}
