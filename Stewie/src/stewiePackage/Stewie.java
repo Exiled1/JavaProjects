c666a95be53658a1840605c4a90411ac72696561
Linuxxx <wazzupbuddy34@gmail.com>
2018-09-10 00:22:27 -0700

    Added @Author tag

--- Stewie/src/stewiePackage/Stewie.java
+++ Stewie/src/stewiePackage/Stewie.java
@@ -1,5 +1,12 @@
 package stewiePackage;
+/*
+@Author Rudy Peralta
 
+    while(WeCantUseVariables = 1){
+        complain.outLoud("Mum, Mum, Mummy, Mum");
+        //There is no escaping the crazy train
+    }
+ */
 public class Stewie {
     public static void main(String[] args) {
         System.out.println("//////////////////////");
