0cf05821e09883344510aeb8246c6b827a7cd831
Linuxxx <wazzupbuddy34@gmail.com>
2018-09-10 00:07:49 -0700

    Added pokeball

---
+++ PokeBall/src/pokeball/PokeBalls.java
@@ -0,0 +1,20 @@
+package pokeball;
+/*
+*@Author Rudy Peralta
+*
+* This Pokeball graphic can be used in the opening
+* sequence of my Pokemon game, or any time a player catches a wild pokemon
+ */
+public class PokeBalls {
+    public static void main(String[] args) {
+        System.out.println("\t    @@@@@@@@@@");
+        System.out.println("\t   @@@@@@@@@@@@");
+        System.out.println("\t  @@@@@@@@@@@@@@");
+        System.out.println("\t @@@@@@@@@@@@@@@@");
+        System.out.println("\t |||||[\"\"\"\"]|||||");
+        System.out.println("\t OOOOOOOOOOOOOOOO");
+        System.out.println("\t  OOOOOOOOOOOOOO");
+        System.out.println("\t   OOOOOOOOOOOO");
+        System.out.println("\t    OOOOOOOOOO");
+    }
+}
