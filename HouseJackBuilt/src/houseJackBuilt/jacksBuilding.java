c89745e926379fc7029d19a139c80aa52d67929b
Linuxxx <wazzupbuddy34@gmail.com>
2018-09-13 01:46:29 -0700

    added last minute files to my homework

---
+++ HouseJackBuilt/src/houseJackBuilt/jacksBuilding.java
@@ -0,0 +1,72 @@
+package houseJackBuilt;
+
+/**
+ * @author rudy peralta
+ */
+public class jacksBuilding {
+    public static void jacksHouseIsBoring() {
+        System.out.println("That lay in the house that Jack built.");
+    }
+
+    public static void funMaltStuff() {
+        System.out.println("That ate the malt");
+    }
+
+    public static void rat() {
+        System.out.println("That killed the rat,");
+    }
+
+    public static void cat() {
+        System.out.println("That worried the cat,");
+    }
+
+    public static void dog() {
+        System.out.println("That tossed the dog,");
+    }
+
+    public static void cow() {
+        System.out.println("That milked the cow with the crumpled horn,");
+    }
+
+    public static void space() {
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is the house that Jack built.");
+        space();
+        System.out.println("This is the malt");
+        jacksHouseIsBoring();
+        space();
+        System.out.println("This is the rat,");
+        funMaltStuff();
+        jacksHouseIsBoring();
+        space();
+        System.out.println("This is the cat,");
+        rat();
+        funMaltStuff();
+        jacksHouseIsBoring();
+        space();
+        System.out.println("This is the dog,");
+        cat();
+        rat();
+        funMaltStuff();
+        jacksHouseIsBoring();
+        space();
+        System.out.println("This is the cow with the crumpled horn,");
+        dog();
+        cat();
+        rat();
+        funMaltStuff();
+        jacksHouseIsBoring();
+        space();
+        System.out.println("This is the maiden all forlorn,");
+        cow();
+        dog();
+        cat();
+        rat();
+        funMaltStuff();
+        jacksHouseIsBoring();
+    }
+}
+
--- SimilarLetters/src/similarLetters/ProblemTwo.java
+++ SimilarLetters/src/similarLetters/ProblemTwo.java
@@ -1,27 +1,34 @@
 package similarLetters;
 
+/**
+ * @author rudy peralta
+ * Again, I had this done in class... I hate myself today.
+ */
 public class ProblemTwo {
 
-	public static void main(String[] args) {
-		LetterOne();
-		LetterTwo();
-		LetterThree();
-	}
-	public static void LetterOne() { // First "Paragraph", uses repeat
-		System.out.println("I like trains");
-		CommonParagraph();
-	}
-	public static void LetterTwo() {// Second "Paragraph", uses repeat
-		System.out.println("She likes animals");
-		CommonParagraph();
-	}
+    public static void main(String[] args) {
+        LetterOne();
+        LetterTwo();
+        LetterThree();
+    }
 
-	public static void LetterThree() {// Third "Paragraph", uses repeat
-		System.out.println("He likes cars.");
-		CommonParagraph();
-	}
-	public static void CommonParagraph() { // The repeating method.
-		System.out.println("Because they're fun");
-	}
-	
+    public static void LetterOne() { // First "Paragraph", uses repeat
+        System.out.println("I like trains");
+        CommonParagraph();
+    }
+
+    public static void LetterTwo() {// Second "Paragraph", uses repeat
+        System.out.println("She likes animals");
+        CommonParagraph();
+    }
+
+    public static void LetterThree() {// Third "Paragraph", uses repeat
+        System.out.println("He likes cars.");
+        CommonParagraph();
+    }
+
+    public static void CommonParagraph() { // The repeating method.
+        System.out.println("Because they're fun");
+    }
+
 }
